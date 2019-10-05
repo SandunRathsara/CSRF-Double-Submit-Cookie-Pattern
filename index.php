c7249c31298b225b434346ee5cc7783b0e16bb63
SandunRathsara <SandunRathsara@users.noreply.github.com>
2019-10-05 22:32:01 +0530

    few edits

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
         <div class="outer_div">
             <h1 class=welcome align=center>Wellcome!</h1>
             <h2 class=welcome align=center>CSRF prevention - Double Submit Cookie Pattern</h3>
-            <br>
             <p class= welcome align=center>Login credentials<br>Username : admin<br>Password : csrf</p>
-            <br>
             <form action="msg.php">
                 <div class=inner_div>
                     <label class=welcome>Username :</label><br><input type="text" class=input_text><br><br>
